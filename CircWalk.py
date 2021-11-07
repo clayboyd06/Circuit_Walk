9a4fffb28e3eb31eea6c766e4327e4f8f32ced04
clayboyd06 <71368259+clayboyd06@users.noreply.github.com>
2021-11-07 14:38:43 -0800

    Update CircWalk.py

--- CircWalk.py
+++ CircWalk.py
@@ -1,3 +1,7 @@
+''' @author Clay Boyd
+    This program uses a random hill-climb algorithm to optimize circuit to fit a parameterized model in LTSpice. 
+    
+'''
 import pickle
 import os
 import time
@@ -9,10 +13,11 @@ from pyltspice import * # poor error handling in the file
 
 
 
-## Setup maybe... maybe every time in runltSpice
+## Setup LTSpice netlist to interract with through program
 netlist = netlist_fromfile(os.path.dirname('/Users/clayboyd/Desktop/Research/Printer/Circuits/dripdrop.asc')+'/dripdrop.net')
 print(netlist)
 
+## Circuit values
 params = {'V1': 100,
           'R1': 1,           
           'C1': 0.00001,
@@ -21,6 +26,7 @@ params = {'V1': 100,
           'D2': '1N750',
           'R2': 10}
 
+## Run the design in ltspice
 def run_ltspice(params):
     for key in params.keys():
         netchange(netlist, param(key, params[key]))
@@ -30,13 +36,16 @@ def run_ltspice(params):
     print('Biggest current:', (max(I_d) - min(I_d)), 'A')
     return I_d, time
 
+## compares the root mean square of the difference between the model and the output of the circuot
 def error(trend):
     t = np.arange(0, 0.05, 1/5000)
+          # temporary trend to get optimization working
+          # TODO create recipe files for optimal circuit outputs 
     ideal_trend = 0.0214*np.sin(2*np.pi*3000*t)
-    return ((I_d - ideal_trend)**2).mean()
+    return ((I_d - ideal_trend)**2).mean() ## TODO update error calculation to account for different shaped waveforms
 
 #======================================================
-
+# visualization 
 I_d, time = run_ltspice(params)
 plt.figure()
 plt.xlim([0, 2.2e-3])
@@ -45,6 +54,7 @@ plt.show()
 
 #IDEAL_CSV = 'idea_trend.csv'
 #ideal_trend = pd.read_csv(IDEAL_CSV)  # TODO ideal_trend.csv <---- generate with module that creates csv from ac, dc, freq, count and 100 values
+##
 
 
 
